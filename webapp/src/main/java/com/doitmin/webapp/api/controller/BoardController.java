6c1b71e8335cf9ff5ccc4d6f20025e505bc5912e
HaeMin_MAC <haemin001212@gmail.com>
2023-12-04 11:00:01 +0900

    [Feat] Springdoc swagger-ui

--- webapp/src/main/java/com/doitmin/webapp/api/controller/BoardController.java
+++ webapp/src/main/java/com/doitmin/webapp/api/controller/BoardController.java
@@ -1,9 +1,11 @@
 package com.doitmin.webapp.api.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "게시판 API")
 @RestController
 @RequestMapping("/api/v1/board")
 public class BoardController {
---
+++ webapp/src/main/java/com/doitmin/webapp/configuration/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.doitmin.webapp.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1-definition")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Doitmin API")
+                        .description("doitmin API description")
+                        .version("v0.0.1"));
+    }
+}
