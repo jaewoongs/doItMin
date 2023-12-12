0548b32719d2cc7a5c138dc9aa57c673f984d81b
jaewoongs <dbswodnd5428@gmail.com>
2023-12-12 19:38:03 +0900

    [Feat] index 모든 기능 화면 구현

--- webapp/src/main/java/com/doitmin/webapp/view/LoginController.java
+++ webapp/src/main/java/com/doitmin/webapp/view/LoginController.java
@@ -4,12 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
     @GetMapping
     public String mainPage(Model model) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:8080/api/v1/"; // Replace with the actual API URL
+        String responseData = restTemplate.getForObject(url, String.class);
+
+        model.addAttribute("data", responseData);
         return "login";
     }
 }
