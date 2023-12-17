ea82ed9241facebf5dcaa14f80845455686ec40b
jaewoongs <dbswodnd5428@gmail.com>
2023-12-17 19:21:26 +0900

    [Feat] 로그인 및 회원가입 api 연동

--- webapp/src/main/java/com/doitmin/webapp/view/LoginController.java
+++ webapp/src/main/java/com/doitmin/webapp/view/LoginController.java
@@ -1,22 +1,158 @@
 package com.doitmin.webapp.view;
 
-import org.springframework.security.access.prepost.PreAuthorize;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+
     @GetMapping
     public String mainPage(Model model) {
-        RestTemplate restTemplate = new RestTemplate();
-        String url = "http://localhost:8080/api/v1/"; // Replace with the actual API URL
-        String responseData = restTemplate.getForObject(url, String.class);
-
-        model.addAttribute("data", responseData);
         return "login";
+    }
+
+    @PostMapping
+    public String handleLogin(@RequestParam String email,
+                              @RequestParam String password, 
+                              HttpServletResponse response,
+                                Model model) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:8080/api/v1/auth/signin"; // Replace with your actual API
+
+        try {
+            LoginRequest loginRequest = new LoginRequest(email, password);
+            ResponseEntity<String> apiResponse = restTemplate.postForEntity(url, loginRequest, String.class);
+             String responseBody = apiResponse.getBody();
+
+             ObjectMapper objectMapper = new ObjectMapper();
+             AuthResponse authResponse = objectMapper.readValue(responseBody, AuthResponse.class);
+             String refreshToken = authResponse.getRefreshToken();
+
+             // 쿠키 생성 및 설정
+             Cookie refreshCookie = new Cookie("refreshToken", refreshToken);
+             refreshCookie.setPath("/"); // 쿠키 접근 경로 설정
+             refreshCookie.setMaxAge(60 * 60 * 24 * 7); // 1주일
+
+             response.addCookie(refreshCookie);
+
+            //  Access Token
+             String accessToken = authResponse.getAccessToken();
+             model.addAttribute("accessToken", accessToken);
+             // Profile API 호출
+            String profileUrl = "http://localhost:8080/api/v1/auth/profile";
+            ResponseEntity<String> profileResponse = restTemplate.exchange(
+                profileUrl, HttpMethod.GET, new HttpEntity<>(createHeaders(authResponse.getAccessToken())), String.class);
+            ProfileResponse userProfile = objectMapper.readValue(profileResponse.getBody(), ProfileResponse.class);
+
+            // 모델에 사용자 프로필 추가
+            model.addAttribute("userProfile", userProfile);
+
+             return "loginSuccess";
+        } catch (Exception e) {
+            model.addAttribute("error", "이메일 또는 비밀번호를 확인해주세요.");
+            return "login";
+        }
+    }
+    private HttpHeaders createHeaders(String accessToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", "Bearer " + accessToken);
+        return headers;
+    }
+
+    static class LoginRequest {
+        private String email;
+        private String password;
+
+        public LoginRequest(String email, String password) {
+            this.email = email;
+            this.password = password;
+        }
+
+        // Getter and setter for email
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        // Getter and setter for password
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+    public static class AuthResponse {
+        private String accessToken;
+        private String refreshToken;
+    
+        public AuthResponse() {}
+    
+        public String getAccessToken() {
+            return accessToken;
+        }
+    
+        public void setAccessToken(String accessToken) {
+            this.accessToken = accessToken;
+        }
+    
+        public String getRefreshToken() {
+            return refreshToken;
+        }
+    
+        public void setRefreshToken(String refreshToken) {
+            this.refreshToken = refreshToken;
+        }
+    }
+    public static class ProfileResponse {
+        private String email;
+        private String nickname;
+        private String[] roleNames;
+    
+        public ProfileResponse() {}
+    
+        public String getEmail() {
+            return email;
+        }
+    
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    
+        public String getNickname() {
+            return nickname;
+        }
+    
+        public void setNickname(String nickname) {
+            this.nickname = nickname;
+        }
+    
+        public String[] getRoleNames() {
+            return roleNames;
+        }
+    
+        public void setRoleNames(String[] roleNames) {
+            this.roleNames = roleNames;
+        }
     }
 }
--- webapp/src/main/java/com/doitmin/webapp/view/SignUpController.java
+++ webapp/src/main/java/com/doitmin/webapp/view/SignUpController.java
@@ -3,13 +3,84 @@ package com.doitmin.webapp.view;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/signup")
 public class SignUpController {
+
     @GetMapping
     public String mainPage(Model model) {
-        return "signup"; // src/main/resources/templates/index.html
+        // Existing code
+        return "signup";
+    }
+
+    @PostMapping
+    public String handleSignup(@RequestParam String email,
+        @RequestParam String password,
+        @RequestParam String confirmPassword,
+        @RequestParam String nickname,
+        Model model, RedirectAttributes redirectAttributes) {
+        if (!password.equals(confirmPassword)) {
+            model.addAttribute("error", "비밀번호 비밀번호 확인이 다릅니다.");
+            return "signup";
+        }
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:8080/api/v1/auth/signup";
+
+        try {
+            SignupRequest signupRequest = new SignupRequest(email, password, nickname);
+            restTemplate.postForEntity(url, signupRequest, String.class);
+
+            redirectAttributes.addFlashAttribute("signupSuccess", true);
+            return "redirect:/signup";
+        } catch (HttpClientErrorException ex) {
+            model.addAttribute("error", "이미 존재하는 이메일입니다.");
+            return "signup";
+        }
+    }
+
+    static class SignupRequest {
+        private String email;
+        private String password;
+        private String nickname;
+
+        public SignupRequest(String email, String password, String nickname) {
+            this.email = email;
+            this.password = password;
+            this.nickname = nickname;
+        }
+
+        // Getters
+        public String getEmail() {
+            return email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public String getNickname() {
+            return nickname;
+        }
+
+        // Setters
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public void setNickname(String nickname) {
+            this.nickname = nickname;
+        }
     }
 }
