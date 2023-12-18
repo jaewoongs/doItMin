338ed3503f21f13937ce6d1af8dc6c7c617a670c
jaewoongs <dbswodnd5428@gmail.com>
2023-12-18 19:37:34 +0900

    [Fix] webapp setting

--- webapp/src/main/java/com/doitmin/webapp/WebappApplication.java
+++ webapp/src/main/java/com/doitmin/webapp/WebappApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.doitmin.webapp"})
 public class WebappApplication extends SpringBootServletInitializer {
 	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(WebappApplication.class);
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WebappApplication.class);
 	}
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(WebappApplication.class);
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
